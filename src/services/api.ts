133febeaedffe3654fc2e97a79a8d52d593998ef
RUDY S. MARPAUNG <rudy.santoso.marpaung@gmail.com>
2026-05-10 21:51:17 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx61pvXIfTZJbkdX4NOdhKCa-Mu8UBQV4WEDh6Xu9APM3HLaX_6CtYlb7QjMgKXdGW2kQ/exec"; 
 
 export interface FormField {
   id: string;
